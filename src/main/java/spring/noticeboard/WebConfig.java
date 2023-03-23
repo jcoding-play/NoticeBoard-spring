1c1e9618e660d5a66a52c94731c7dfbf42d1f280
gyungchan Jo <namejgc@naver.com>
2023-03-23 14:55:22 +0900

    feat: 로그인 체크 인터셉터 구현

---
+++ src/main/java/spring/noticeboard/WebConfig.java
@@ -0,0 +1,18 @@
+package spring.noticeboard;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.noticeboard.web.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+}
